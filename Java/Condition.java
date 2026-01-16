7e6fa14271b80fc214ecb3b9f41f521dece9f840
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-16 19:38:21 +0600

    Condition Tags

--- Java/Condition.java
+++ Java/Condition.java
@@ -4,7 +4,14 @@ public class Condition {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
-        
+        int x = sc.nextInt();
+
+        if(x % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
         if (age >= 18) {
             System.out.println("Adult");
         

70e60351c5d24f48e933e0873fd89556f1f26322
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-16 16:37:12 +0600

    Additon, Substraction and Murltiplication by java

---
+++ Java/Additionandsubstraction.java
@@ -0,0 +1,23 @@
+public class Additionandsubstraction {
+      public static void main(String[] args) {
+          //variables
+          String name = "Alex Zravo";
+          int age = 21;
+          double price = 20.20;
+
+          int a = 10;
+          int b = 20;
+          
+          int sum = a + b;
+          int diff = b - a;
+          int mul = a * b;
+          int ans = (a * b) / (a - b);
+
+          System.out.println("Sum = " + sum);
+          System.out.println("Difference = " + diff);
+          System.out.println("Multiplication = " + mul);
+          System.out.println("Question Answer = " + ans);
+          System.out.println("About Zravo = Name : " + name + " And Age : " + age);
+          
+      }
+}

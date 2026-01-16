23410603cf4a298a41eaf450a800980afda76091
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-16 15:46:22 +0600

    Java Line Switcher tag

---
+++ Java/Main1.java
@@ -0,0 +1,12 @@
+public class Main1 {
+       public static void main(String[] args) {
+        //output
+           System.out.println("*");
+           System.out.println("**");
+           System.out.println("***");
+       
+
+    System.out.print("*\n**\n***");
+       }
+
+}

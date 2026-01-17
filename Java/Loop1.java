e9f0e6ac088a4b32fcebf0a4b34e9890264b0780
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-17 23:45:02 +0600

    Loops 1-1000 Count

---
+++ Java/Loop1.java
@@ -0,0 +1,10 @@
+
+
+public class Loop1 {
+
+    public static void main(String args[] ) {
+        for (int counter = 0; counter <1001; counter++) {
+            System.out.println(counter);
+        }
+        }
+    }

8708db9d8e7d807ed396f3538e189b8e7e8177a3
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-18 01:15:07 +0600

    Do While Loops

--- Java/Loopcondition.java
+++ Java/Loopcondition.java
@@ -1,10 +1,29 @@
 public class Loopcondition {
     public static void main(String[] args) {
-       int i = 0;
-       while (i < 11) {
-        System.out.println(i);
-        i = i + 3;
+
+        // while loops =>
+
+       //int i = 0;
+       //while (i < 11) {
+        //System.out.println(i);
+       // i = i + 3; // i += 3
+
+       // do while loops =>
+
+      // int i = 0;
+      // do { 
+       // System.out.println(i);
+       // i = i + 1;
+        // } while (i < 11);
+
+        // question => print the sum of first n natural number ; if n = 4 given
+        // solution =>
        
+        
+        for(int i = 1; i <= 4; i++) {
+            System.out.println(i);
         }
+
+       }
     }
-}
+

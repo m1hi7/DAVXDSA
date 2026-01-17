df6ddd7708100fd5556bc08541c6ce8e06b94731
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-18 04:57:01 +0600

    Half Pyramid with Numbers (pattern6)

---
+++ Java/Pattern6.java
@@ -0,0 +1,19 @@
+public class Pattern6 {
+
+    public static void main(String[] args ) {
+
+        //Problems 6 : Half Pyramid with numbers
+        
+        int n = 5;
+
+        // outer loop
+            for(int i=1; i<=n; i++) {
+                // inner loop
+                for(int j=1; j<=i; j++) {
+                    System.out.print(j + " ");
+                }
+                 System.out.println();
+         }
+            }
+            
+    }

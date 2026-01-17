b8a1412272db55b6bb408c19e0c3ee2661901a5b
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-17 23:59:28 +0600

    Loop Count with while

--- Java/Loop1.java
+++ Java/Loop1.java
@@ -4,8 +4,12 @@ public class Loop1 {
 
     public static void main(String args[] ) {
         //counter++ => counter = counter + 1
-        for (int counter = 0; counter <1001; counter++) {
-            System.out.println(counter);
-        }
+        //for (int counter = 0; counter <1001; counter++) {
+          //  System.out.println(counter);
+        
+        
+        for (int i = 0; i <=1000; i++) {
+            System.out.println(i);
         }
     }
+}

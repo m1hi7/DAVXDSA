e9c4bea33da03611eb60879693068595b9d6f8e8
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-17 23:50:43 +0600

    Loop count

--- Java/Loop1.java
+++ Java/Loop1.java
@@ -3,6 +3,7 @@
 public class Loop1 {
 
     public static void main(String args[] ) {
+        //counter++ => counter = counter + 1
         for (int counter = 0; counter <1001; counter++) {
             System.out.println(counter);
         }

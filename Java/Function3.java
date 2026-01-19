ffa76ff0fc5d574b44eaf073df247b27b54da5d9
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-19 22:36:43 +0600

    The Factorial of a Number

---
+++ Java/Function3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Function3 {
+    public static void printFactorial(int n) {
+     //loop
+     if(n < 0) {
+        System.out.println("Invalid Number");
+        return;
+     }
+     int factorial = 1;
+
+     for(int i=n; i>=1; i--) {
+        factorial = factorial * i;
+     }
+     System.out.println(factorial);
+     
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        printFactorial(num);
+     }    
+}

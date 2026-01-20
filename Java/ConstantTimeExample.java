95e716eec83ae2eb41ebe2fd21a7dbe8ccaa2355
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-20 22:37:45 +0600

    Constant Time Example

---
+++ Java/ConstantTimeExample.java
@@ -0,0 +1,8 @@
+public class ConstantTimeExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Always one operation
+        System.out.println("First element: " + numbers[0]);
+    }
+}

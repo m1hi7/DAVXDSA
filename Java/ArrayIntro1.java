a1b50e39e114f26e7ba0b0a01212705686cafca1
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-21 23:31:15 +0600

    Into Of Array

---
+++ Java/ArrayIntro1.java
@@ -0,0 +1,15 @@
+public class ArrayIntro1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[5]; // declare an array of size 5
+
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + " = " + numbers[i]);
+        }
+    }
+}

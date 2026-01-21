ef3214b3ca3f52545a9943b4cc11f3a62ddd524e
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-21 23:33:47 +0600

    Shorter Initialization with Values

---
+++ Java/ArrayIntro2.java
@@ -0,0 +1,10 @@
+public class ArrayIntro2 {
+    public static void main(String[] args) {
+        int[] primes = {2, 3, 5, 7, 11};
+
+        System.out.println("Array elements:");
+        for (int value : primes) {
+            System.out.println(value);
+        }
+    }
+}

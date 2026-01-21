294fb2c0795b7b3b82b6e182c017f03aefebf79b
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-21 23:38:19 +0600

    Traversing And Printing with Indexed Loop (Array)

---
+++ Java/ArrayIntro4.java
@@ -0,0 +1,9 @@
+public class ArrayIntro4 {
+    public static void main(String[] args) {
+        String[] names = {"Alice", "Bob", "Charlie", "Dave"};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Name at index " + i + ": " + names[i]);
+        }
+    }
+}

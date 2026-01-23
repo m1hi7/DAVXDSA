aba64d17d0ddf18418aa85b0f1ac1e1dd1e1f7d4
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-23 23:17:18 +0600

    String Comparison Example

---
+++ Java/String3.java
@@ -0,0 +1,7 @@
+public class String3 {
+    public static void main(String[] args) {
+        String word = "Programming";
+        System.out.println(word.charAt(0));
+        System.out.println(word.charAt(3));
+    }
+}
---
+++ Java/String4.java
@@ -0,0 +1,11 @@
+public class String4 {
+    public static void main(String[] args) {
+        String a = "Java";
+        String b = "Java";
+        String c = new String("Java");
+
+        System.out.println(a == b);
+        System.out.println(a == c);
+        System.out.println(a.equals(c));
+    }
+}

bf93aed27db98e96d2487fbc2d7cd10dff192e47
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-23 23:20:17 +0600

    String Replace Example

---
+++ Java/String6.java
@@ -0,0 +1,6 @@
+public class String6 {
+    public static void main(String[] args) {
+        String text = "I love Java";
+        System.out.println(text.replace("Java", "Programming"));
+    }
+}

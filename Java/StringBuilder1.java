a5edf7d7aaf43277f68043bc82bcc31bfbec8751
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-23 23:26:37 +0600

    Delete Char At Some Index

---
+++ Java/StringBuilder1.java
@@ -0,0 +1,15 @@
+
+
+public class StringBuilder1 {
+   public static void main(String args[]) {
+     StringBuilder sb = new StringBuilder("tony");
+     //Insert char
+     sb.insert(0, 'S');
+     System.out.println(sb);
+
+
+	//delete char
+     sb.delete(0, 1);
+     System.out.println(sb);
+   }
+}

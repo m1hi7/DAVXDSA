18ff683bf72456c64b37871e39238f92706fc3f7
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-24 23:35:27 +0600

    Clear Bit

---
+++ Java/Bits2.java
@@ -0,0 +1,13 @@
+public class Bits2 {
+     
+     public static void main(String[] args) {
+         int n = 5;
+         int pos = 2;
+         int bitMask = 1<<pos;
+         int notBitMask = ~(bitMask);
+
+        int newNumber = n & notBitMask;
+         System.out.println("Number after clearing bit at position " + pos + " : "   + newNumber);
+
+     }
+}

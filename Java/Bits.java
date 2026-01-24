5adacf77429085323d328fee5826fe6b05cfe0f9
m1hi7 <mahin.a.b.223@gmail.com>
2026-01-24 23:27:08 +0600

    Bits Begening

---
+++ Java/Bits.java
@@ -0,0 +1,15 @@
+
+public class Bits {
+     
+     public static void main(String[] args) {
+         int n = 5;
+         int pos = 2;
+         int bitMask = 1<<pos;
+
+         if((n & bitMask) == 0) {
+             System.out.println("Bit was zero");
+         } else {
+             System.out.println("Bit was one");
+         }
+     }
+}

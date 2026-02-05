ec7826e77f83b3903f663dd63fc372f7d76261c5
m1hi7 <mahin.a.b.223@gmail.com>
2026-02-05 23:02:14 +0600

    Scratch Implementation (Important for BEGINNERS)

---
+++ Java/LL1.java
@@ -0,0 +1,108 @@
+class LL1 {
+
+
+   Node head;
+   private int size;
+
+
+   LL1 () {
+       size = 0;
+   }
+
+
+   public class Node {
+       String data;
+       Node next;
+
+
+       Node(String data) {
+           this.data = data;
+           this.next = null;
+           size++;
+       }
+   }
+
+
+   public void addFirst(String data) {
+       Node newNode = new Node(data);
+       newNode.next = head;
+       head = newNode;
+   }
+
+
+   public void addLast(String data) {
+       Node newNode = new Node(data);
+
+
+       if(head == null) {
+           head = newNode;
+           return;
+       }
+
+
+       Node lastNode = head;
+       while(lastNode.next != null) {
+           lastNode = lastNode.next;
+       }
+
+
+       lastNode.next = newNode;
+   }
+
+
+   public void printList() {
+       Node currNode = head;
+
+
+       while(currNode != null) {
+           System.out.print(currNode.data+" -> ");
+           currNode = currNode.next;
+       }
+
+
+       System.out.println("null");
+   }
+
+
+   public void removeFirst() {
+       if(head == null) {
+           System.out.println("Empty List, nothing to delete");
+           return;
+       }
+
+
+       head = this.head.next;
+       size--;
+   }
+
+
+   public void removeLast() {
+       if(head == null) {
+           System.out.println("Empty List, nothing to delete");
+           return;
+       }
+
+
+       size--;
+       if(head.next == null) {
+           head = null;
+           return;
+       }
+
+
+       Node currNode = head;
+       Node lastNode = head.next;
+       while(lastNode.next != null) {
+           currNode = currNode.next;
+           lastNode = lastNode.next;
+       }
+
+
+       currNode.next = null;
+   }
+
+
+   public int getSize() {
+       return size;
+   }
+}

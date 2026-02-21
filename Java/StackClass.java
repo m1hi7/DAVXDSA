97d875780066481e92827534db2e66c627d39911
m1hi7 <mahin.a.b.223@gmail.com>
2026-02-21 17:40:15 +0600

    Stack Class two

--- Java/StackClass.java
+++ Java/StackClass.java
@@ -1,55 +1,56 @@
+public class StackClass {
 
-import java.util.Stack;
+    static class Node {
+        int data;
+        Node next;
 
-public class StackClass { 
-        static class node {
-            int data;
-            node next;
-            public node(int data) {
-                this.data = data;
-                this.next = null;
-            }
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
-        @SuppressWarnings("unused")
-        static class stack {
-            public node head;
-            public boolean isEmpty() {
-                return head == null;
-            }
-            public void push(int data) {
-                node newNode = new node(data);
-                if(isEmpty()) {
-                    head = newNode;
-                    return;
-                }
-                newNode.next = head;
-                head = newNode;
-            }
-            public int pop() {
-                if(isEmpty()) {
-                    return -1;
-                }
-                int top = head.data;
-                head = head.next;
-                return top;
-            }
-            public int peek() {
-                if(isEmpty()) {
-                    return -1;
-                }
-                return head.data;
-            }
-            public static void main(String[] args) {
-                Stack s = new Stack();
-                s.push(1);
-                s.push(2);  
-                s.push(3);
-                s.push(4);
+    }
 
-                while(!s.isEmpty()) {
-                    System.out.println(s.peek());
-                    s.pop();
-                }
-            }
+    static class Stack {
+        public Node head;
+
+        public boolean isEmpty() {
+            return head == null;
         }
+
+        public void push(int data) {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
 }
--- Java/StackClass1.java
+++ Java/StackClass1.java
@@ -1,55 +1,51 @@
 import java.util.ArrayList;
 
 public class StackClass1 {
-             static class Stack {
-               static ArrayList<Integer> list = new ArrayList<>();
 
-        public static ArrayList getList() {
-            return list;
+    static class Stack {
+
+        private ArrayList<Integer> list = new ArrayList<>();
+
+        public boolean isEmpty() {
+            return list.isEmpty();
         }
 
-        public static void setList(ArrayList list) {
-            Stack.list = list;
-        }
-                public boolean isEmpty() {
-                    return list.isEmpty();
-                }
-
-                //Push
-                public void push(int data) {
-                    list.add(data);
-                }
-
-                //pop
-                public int pop() {
-                    if(isEmpty()) {
-                        return -1;
-                    }
-                    int top = list.get(list.size() - 1);
-                    list.remove(list.size() - 1);
-                    return top;
-                }
-
-                //peek
-                public int peek() {
-                    if(isEmpty()) {
-                        return -1;
-                    }
-                    return list.get(list.size()-1);
-                }
-
-                public static void main(String[] args) {
-                    Stack s = new Stack();
-                    s.push(1);
-                    s.push(2);  
-                    s.push(3);
-                    s.push(4);
-
-                    while(!s.isEmpty()) {
-                        System.out.println(s.peek());
-                        s.pop();
-                    }
+        // Push
+        public void push(int data) {
+            list.add(data);
         }
 
-               }
+        // Pop
+        public int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        // Peek
+        public int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Stack s = new Stack();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
 }

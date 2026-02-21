c92d9af10f884f9168c23e3e9050f1c0cef79351
m1hi7 <mahin.a.b.223@gmail.com>
2026-02-21 17:09:45 +0600

    Stack Class

---
+++ Java/StackClass.java
@@ -0,0 +1,54 @@
+
+import java.util.Stack;
+
+public class StackClass { 
+        static class node {
+            int data;
+            node next;
+            public node(int data) {
+                this.data = data;
+                this.next = null;
+            }
+        }
+        static class stack {
+            public node head;
+            public boolean isEmpty() {
+                return head == null;
+            }
+            public void push(int data) {
+                node newNode = new node(data);
+                if(isEmpty()) {
+                    head = newNode;
+                    return;
+                }
+                newNode.next = head;
+                head = newNode;
+            }
+            public int pop() {
+                if(isEmpty()) {
+                    return -1;
+                }
+                int top = head.data;
+                head = head.next;
+                return top;
+            }
+            public int peek() {
+                if(isEmpty()) {
+                    return -1;
+                }
+                return head.data;
+            }
+            public static void main(String[] args) {
+                Stack s = new Stack();
+                s.push(1);
+                s.push(2);  
+                s.push(3);
+                s.push(4);
+
+                while(!s.isEmpty()) {
+                    System.out.println(s.peek());
+                    s.pop();
+                }
+            }
+        }
+}

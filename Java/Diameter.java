c115eff6e0f5001f9a9bcee3d3ce00ffa90b7cab
m1hi7 <mahin.a.b.223@gmail.com>
2026-02-25 23:13:04 +0600

    Diameter of tree - Approch ON)

--- Java/Diameter.java
+++ Java/Diameter.java
@@ -1,4 +1,11 @@
-public static int diameter(Node root) {
+public static int height(Node root) {
+       if(root == null) {
+           return 0;
+       }
+       return Math.max(height(root.left), height(root.right)) + 1;
+   }
+
+   public static int diameter(Node root) {
        if(root == null) {
            return 0;
        }

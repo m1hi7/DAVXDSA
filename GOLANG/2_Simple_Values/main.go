375f5438d5d38e13e461c74cabbe8e7ebdc92d7c
m1hi7 <mahin.a.b.223@gmail.com>
2026-03-12 12:29:55 +0600

    Simple values on Go

---
+++ GOLANG/2_Simple_Values/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values ->
+
+	// integers
+	fmt.Println(1)
+
+	// strings
+	fmt.Println("Hello, Go!")
+
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	//floats
+	fmt.Println(10.5)
+	fmt.Println(7.0 / 3.0)
+}

7d300e502f2e78ed9b19952b7ad6c638c866290b
m1hi7 <mahin.a.b.223@gmail.com>
2026-03-12 12:51:14 +0600

    Variables in Go

---
+++ GOLANG/3_Variables/variables.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//1 -> string
+	var name = "Go"
+	fmt.Println(name)
+
+	//2 -> bool
+	var age int = 10
+	fmt.Println(age)
+
+	//3 -> shorthand syntax
+	//name := "Golang"
+	//fmt.Println(name)
+
+	//4 -> string where it must have to use
+	//var name string
+	//name = "Golang"
+	//fmt.Println(name)
+
+	//var price float64 = 50.5
+	//fmt.Println(price)
+	//Alt
+	//price := 40.5
+	//fmt.Println(price)
+}

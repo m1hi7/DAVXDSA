1ef40bef6d0aae3377804e77c5da01e7223d2057
m1hi7 <mahin.a.b.223@gmail.com>
2026-03-15 19:32:12 +0600

    string in arrays

---
+++ GOLANG/8_Arrays/arrays2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [3]string
+	names[0] = "Abdullah"
+	names[1] = "petunia"
+	names[2] = "HJIRM"
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+}

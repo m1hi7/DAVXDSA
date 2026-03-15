7f88bbd3e1513ff7ced7bf43ecc72cf2f854a6db
m1hi7 <mahin.a.b.223@gmail.com>
2026-03-16 05:45:18 +0600

    slices in go

---
+++ GOLANG/9_slices/slices.go
@@ -0,0 +1,12 @@
+package main
+
+//slice
+//mostly used construct in go
+//+ useful methods
+//+ dynamic size
+//+ can be used as a stack, queue, list, etc
+func main() {
+	// uninitialized slice is nil
+	var nums []int
+	println(len(nums))
+}
